20141b0dcbdb6c0455c9323a915fb387f0351090
Alessandro Mezzogori <alessandro.mezzogori1802@gmail.com>
2024-01-14 03:22:24 +0100

    fix ignored log lib files

---
+++ ChillHttp/log/log.c
@@ -0,0 +1,43 @@
+#include "log.h"
+
+const enum LOG_LEVEL log_runtime_level = LL_TRACE;
+const char* log_level_strings[] = {
+	"NONE",
+	"TRA",
+	"DBG",
+	"INF",
+	"WRN",
+	"ERR",
+	"FTL"
+};
+
+int logger(FILE* out, const enum LOG_LEVEL lvl, char* const func, const unsigned int line, char* const fmt, ...) {
+	// Building loggin timestamp
+	time_t t = time(NULL);
+	struct tm* tm = localtime(&t);
+	char time[64];
+	strftime(time, sizeof(time), "%c", tm);
+
+	// building passed format with appropriate variadic params
+	char const buffer[2048];
+	memset(buffer, 0, sizeof(buffer));
+
+	va_list argptr;
+	va_start(argptr, fmt);
+	// TODO handle buffer overflow
+	vsprintf_s(buffer, sizeof(buffer), fmt, argptr);
+	va_end(argptr);
+
+	// Printing actual log
+	fprintf_s(out, "[%s] %s: %s:%d %s\n", log_level_strings[lvl], time, func, line, buffer);
+
+	// Flushing log file
+	// Not super efficient, but it's for ease of use 
+	flushLogger(out);
+
+	return 0;
+}
+
+void flushLogger(FILE* out) {
+	fflush(out);
+}
---
+++ ChillHttp/log/log.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <wchar.h>
+#include <varargs.h>
+#include <time.h>
+
+enum LOG_LEVEL {
+	LL_NONE		= 0,
+	LL_TRACE	= 1,
+	LL_DEBUG	= 2,
+	LL_INFO		= 3,
+	LL_WARN		= 4,
+	LL_ERROR	= 5,
+	LL_FATAL	= 6,
+};
+
+extern const enum LOG_LEVEL log_runtime_level;
+int logger(FILE* out, const enum LOG_LEVEL lvl, const char* func, const unsigned int line, const char* fmt, ...);
+void flushLogger(FILE* out);
+
+#ifndef LOG_BUILD_LEVEL
+	#ifdef DEBUG
+		#define LOG_BUILD_LEVEL LL_TRACE
+	#else
+		#define LOG_BUILD_LEVEL LL_TRACE
+	#endif // DEBUG
+#endif // !LOG_BUILD_LEVEL
+
+#define SHOULD_LOG(lvl) ( lvl >= log_runtime_level && lvl >= LOG_BUILD_LEVEL)
+
+#ifndef LOG_FP
+	#ifdef stdout
+		#define LOG_FP stdout
+	#endif // stdout
+#endif // !LOG_FP
+
+#define LOG(level, fmt, ...) do { \
+	if (SHOULD_LOG(level)) { \
+		logger(LOG_FP, level, __func__, __LINE__, fmt, __VA_ARGS__); \
+	} \
+} while(0)
+
+#define LOG_TRACE(fmt, ...) LOG(LL_TRACE, fmt, __VA_ARGS__) 
+#define LOG_DEBUG(fmt, ...) LOG(LL_DEBUG, fmt, __VA_ARGS__)
+#define LOG_INFO(fmt, ...) LOG(LL_INFO, fmt, __VA_ARGS__)
+#define LOG_WARN(fmt, ...) LOG(LL_WARN, fmt, __VA_ARGS__)
+#define LOG_ERROR(fmt, ...) LOG(LL_ERROR, fmt, __VA_ARGS__)	
+#define LOG_FATAL(fmt, ...) LOG(LL_FATAL, fmt, __VA_ARGS__)	
+#define LOG_FLUSH() flushLogger(LOG_FP)
+
